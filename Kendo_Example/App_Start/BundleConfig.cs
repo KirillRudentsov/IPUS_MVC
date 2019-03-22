2459602b21bec96e24c21beab4a03d4ced39f422
Kirill <krudentsov@mail.ru>
2019-03-22 19:46:15 +0300

    add

--- Kendo_Example/App_Start/BundleConfig.cs
+++ Kendo_Example/App_Start/BundleConfig.cs
@@ -26,7 +26,7 @@ namespace Kendo_Example
 
             bundles.Add(new ScriptBundle("~/Scripts/kendo").Include(
             "~/Scripts/kendo/2019.1.220/kendo.all.min.js",
-            // "~/Scripts/kendo/kendo.timezones.min.js", // uncomment if using the Scheduler
+            "~/Scripts/kendo/2019.1.220/kendo.timezones.min.js",
             "~/Scripts/kendo/2019.1.220/kendo.aspnetmvc.min.js"));
 
             bundles.Add(new ScriptBundle("~/Scripts/Custom").Include(
@@ -34,6 +34,9 @@ namespace Kendo_Example
 
             bundles.Add(new ScriptBundle("~/Scripts/Cultures").Include(
                 "~/Scripts/Cultures/kendo.culture.ru-RU.min.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/messages").Include(
+                "~/Scripts/Cultures/kendo.messages.ru-RU.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/kendo").Include(
             "~/Content/kendo/2019.1.220/kendo.common.min.css",
