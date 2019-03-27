4184b624a1b1b3b776f10484f88223853fdf5581
Kirill <krudentsov@mail.ru>
2019-03-27 15:47:47 +0300

    add DB class library

---
+++ Kendo_Example/SupportClasses/KendoSQLBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+
+namespace Kendo_Example.SupportClasses
+{
+    public class KendoSQLBuilder
+    {
+        //public static string BuildFilterableAndSortableSQLQuery(string sql, DataSourceRequest request, )
+        //{
+
+        //}
+    }
+}
---
+++ MainServer/Extension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainServer.Extension
+{
+    class Extension
+    {
+    }
+
+    public class ProcedureParameter
+    {
+        public string ParameterName;
+        public string ParameterValue;
+        public string ParameterType;
+        public string ParameterDirection;
+
+        public ProcedureParameter() { }
+
+        public ProcedureParameter(string paramName, string paramVal, string paramType, string paramDir)
+        {
+            ParameterName = paramName;
+            ParameterValue = paramVal;
+            ParameterType = paramType;
+            ParameterDirection = paramDir;
+        }
+    }
+}
